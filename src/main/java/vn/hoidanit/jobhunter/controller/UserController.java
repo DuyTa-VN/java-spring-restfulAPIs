fe16d0699120c58ff368931c3235aafc6e8f0482
DuyTa-VN <duydt202@gmail.com>
2025-02-24 08:18:02 +0700

    video 33

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,7 +1,13 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +24,33 @@ public class UserController {
     }
 
     // @GetMapping("/user/create")
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User postManUser) {
         User ericUser = this.userService.handleCreateUser(postManUser);
 
+        return ericUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "ericUser";
+    }
+
+    // fetch user by id
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id) {
+        return this.userService.fetchUserById(id);
+    }
+
+    @GetMapping("/user")
+    public List<User> fetchAllUsers() {
+        return this.userService.fetchAllUser();
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        User ericUser = this.userService.handleUpdateUser(user);
         return ericUser;
     }
 }
